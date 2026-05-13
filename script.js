4e294f4050111fe47e530abde3e5566122535c35
sapucai07 <samiraceliz07@gmail.com>
2026-05-13 19:23:37 -0300

    Paso 8

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ fetch("https://rickandmortyapi.com/api/character")
     .then(respuesta => respuesta.json())
     .then(datos => {
         datos.results.forEach(personaje => {
+            document.getElementById("personajes").innerHTML += `
+<div class="col-md-3 mb-4">
+<div class="card">
+<img src="${personaje.image}" class="card-img-top">
+<div class="card-body">
+<h5 class="card-title">
+${personaje.name}
+</h5>
+<p>
+${personaje.species}
+</p>
+</div>
+</div>
+</div>
 
-            console.log(personaje.name);
+`;
         });
     });
